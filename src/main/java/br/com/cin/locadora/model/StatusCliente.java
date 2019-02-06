6dd565ed0fca65944b3eb084c67401855ba470a5
Fagner Costa <cin.ufpe.br>
2019-02-05 21:18:25 -0400

    teste

--- src/main/java/br/com/cin/locadora/model/StatusCliente.java
+++ src/main/java/br/com/cin/locadora/model/StatusCliente.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Basic;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -27,12 +28,11 @@ public class StatusCliente implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Basic(optional = false)
     @Column(name = "id_status")
     private Integer idStatus;
     @Column(name = "descricao")
     private String descricao;
-    @OneToMany(mappedBy = "status")
+    @OneToMany(mappedBy = "status",cascade= {CascadeType.ALL}, fetch=FetchType.LAZY)
     private List<Cliente> clienteList = new ArrayList<Cliente>();
 
     public StatusCliente() {
